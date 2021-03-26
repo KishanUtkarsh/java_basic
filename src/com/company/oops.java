e07f0b60851a80a646399d62d52187d5c0ff51e4
KishanUtkarsh <kishan.utkarsh12082002@gmail.com>
2021-03-26 23:46:44 +0530

    learning loop

---
+++ src/com/company/oops.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class oops{
+    static void SayHello(){
+        System.out.println("Hello MotherFucker!");
+    }
+    static void printname(String s){
+        System.out.println("your name is "+s);
+    }
+    public static void main(String[] args){
+        SayHello(); // we can use sayhello method many time as we want.
+        SayHello();
+        printname("Ankit.");
+    }
+}
---
+++ src/com/company/return_type.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class return_type {
+
+    static int sum(int a, int b){
+        return a+b;
+
+    }
+    public static void main(String[] args){
+        int x = sum(5,4);
+        System.out.println(x);
+    }
+}
