4899d2bf20d92f7e4fc3ef357f2c2391652be537
KishanUtkarsh <kishan.utkarsh12082002@gmail.com>
2021-04-08 01:28:32 +0530

    exception handling with throws method

---
+++ src/com/company/Array_list.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Array_list {
+    public static void main(String...args) {
+        ArrayList<Integer> colorList = new ArrayList(10);
+
+
+
+        colorList.addAll(Arrays.asList(4,5,6));
+
+    }
+}
