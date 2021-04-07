fe8926972e0725e783e9b3ced1602f8437b6c926
KishanUtkarsh <kishan.utkarsh12082002@gmail.com>
2021-04-07 16:46:43 +0530

    exception handling with throws method

---
+++ src/com/company/exception_handling.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+class MyCalculator {
+
+    long power(int a , int b)throws Exception{
+        if (a == 0 && b == 0){
+            throw new Exception("n and p should not be zero.");
+        }
+        else if (a < 0 || b < 0){
+            throw new Exception("n or p should not be negative.");
+        }
+        else{
+
+            // int d = 1;
+            // while (b > 0){
+
+            //     d = d*a;
+            //     b -= 1 ;
+            // }
+            // return d;
+
+            return (int) Math.pow(a,b);
+        }
+    }
+}
+public class exception_handling {
+    public static void main(String[] args){
+        final Scanner sc = new Scanner(System.in);
+        final MyCalculator mc = new MyCalculator();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        try {
+            System.out.println(mc.power(x ,y));
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
